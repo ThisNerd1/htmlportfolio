8a93049ffc704b8e0cb41efa6f072d3bcd8d263e
Daijah Coppage <89088157+ThisNerd1@users.noreply.github.com>
2025-05-26 12:54:20 -0600

    frontend almost set
    Project section and projects next.

--- Animation/home.js
+++ Animation/home.js
@@ -2,42 +2,43 @@ function changeColor(colorClass) {
   var element = document.body;
   element.classList.remove('daydreams', 'chocolate', 'cool', 'cotton-candy');
   element.classList.add(colorClass);
-  if (element.style.backgroundColor === 'chocolate') {
-    document.querySelector('.coffee').style.display = 'block';
+  if (colorClass === 'chocolate') {
+    var barColors = '#874B35';
+  } else if (colorClass === 'daydreams'){
+    var barColors = '#F2F3F5';
+  } else if (colorClass === 'cool'){
+    var barColors = '#CCCCFF';
   } else {
-    document.querySelector('.coffee').style.display = 'none';
+    barColors = '#A3A1A1';
   }
+  const xValues = ["Golang", "React", "C#", "Python", "Angular", "React-Native","PHP", "Javascript"];
+  const yValues = [3.5, 5.5, 7, 6, 6, 5, 4, 7.5];
+
+new Chart("skillChart", {
+  type: "bar",
+  data: {
+    labels: xValues, 
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true, 
+          max: 10, 
+          color: '#F2F3F5',
+        }
+      }]
+    },
+    title: {
+      display: true,
+      text: "1 - Lowest, 10 - Highest"
+    }
+  }
+});
 }
 
-
-
-let chart = anychart.heatMap(getData());
-chart.background().fill("rgba(0,0,0,0)");
-chart.xAxis().labels().fontColor("#FFFFFF"); 
-chart.yAxis().labels().fontColor("#FFFFFF");
-chart.container("container");
-chart.draw();
-
-
-// x-axis = on top
-// y-axis = left side
-
-function getData() {
-  return [
-    {
-      x: "Javascript",
-      y: "Confidence Level",
-      heat: 705
-    },
-    {
-      x: "React",
-      y: "Confidence Level",
-      heat: 705
-    },
-    {
-      x: "C#",
-      y: "Confidence Level",
-      heat: 100
-    } 
-  ];
-}
